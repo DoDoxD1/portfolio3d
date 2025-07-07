ae92c648cbee37560c1485137083acaaec8a57c5
= <=>
2025-07-07 19:03:17 +0530

    complete work experience section

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,13 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+// import meta from "./company/meta.png";
+// import shopify from "./company/shopify.png";
+// import starbucks from "./company/starbucks.png";
+// import tesla from "./company/tesla.png";
+import carelon from "./company/carelon.png";
+import wipro from "./company/wipro.png";
+import readymotive from "./company/readymotive.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +55,9 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  carelon,
+  wipro,
+  readymotive,
   carrent,
   jobit,
   tripguide,
